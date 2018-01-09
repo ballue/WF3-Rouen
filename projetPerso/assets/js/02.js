ee7a532f55aff75241f10a1c6842d6dbc493b6ff
ballue <jerome.ballue@gmail.com>
2018-01-09 10:14:15 +0100

    Ajout de ce truc

---
+++ projetPerso/assets/js/02.js
@@ -0,0 +1,30 @@
+var PremierTrimestre = [
+    {
+        nom       :   "LIEGEARD",
+        prenom    :   "Hugo",
+        moyenne   :   {
+                            francais : 4,
+                            math     : 8,
+                            physique : 18
+                        }
+    },
+    {
+        nom       :   "MANAS",
+        prenom    :   "Tanguy",
+        moyenne   :   {
+                            francais : 6,
+                            math     : 15,
+                            physique : 9,
+                            anglais  : 15.5
+                        }
+    },
+    {
+        nom       :   "ARAUJO",
+        prenom    :   "Thiago",
+        moyenne   :   {
+                            francais : 10,
+                            math     : 15,
+                            physique : 16
+                        }
+    }
+];
